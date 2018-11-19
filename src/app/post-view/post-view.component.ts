7cbf414a673baf35c4897fedccc84d88c87b188b
Pedro Lamkowski <pedrolamkowski@gmail.com>
2018-11-19 19:32:54 -0200

    fazendo report button

--- src/app/post-view/post-view.component.ts
+++ src/app/post-view/post-view.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
 export interface Post {
   author: string;
   text: string;
   date: firebase.firestore.Timestamp;
   title: string;
+  reports: string;
 }
 @Component({
   selector: 'app-post-view',
@@ -14,18 +16,24 @@ export interface Post {
   styleUrls: ['./post-view.component.css']
 })
 export class PostViewComponent implements OnInit {
+  currentId: string;
   info: Observable<Post>;
   collection: AngularFirestoreCollection<Post>;
   constructor(private route: ActivatedRoute, db: AngularFirestore) {
-    this.collection = db.collection<Post>("posts");
+    this.collection = db.collection<Post>('posts');
 
   }
 
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
-    console.log(id);
-    this.info = this.collection.doc<Post>("/"+id).valueChanges();
+    this.currentId = id;
+    this.info = this.collection.doc<Post>('/' + id).valueChanges();
+  }
+
+  reportButton(): void {
+    const d = this.collection.doc<Post>('/' + this.currentId).snapshotChanges().pipe(map(arr => {
+    })).subscribe();
   }
 
 }
